e3a21381bd6f6541f34a633f04058fff970d59d1
Chingis Choybsonov <avatarius93@gmail.com>
2024-01-19 02:20:56 +0300

    первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// Функция создания карточки
+function addCard(cardObj, removeFunc) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  let {name, link} = cardObj;
 
-// @todo: Функция создания карточки
+  const title = cardElement.querySelector('.card__title');
+  const img = cardElement.querySelector('.card__image');
+  const removeBtn = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  title.textContent = name;
+  img.alt = name;
+  img.src = link;
+  removeBtn.addEventListener('click', (event) => removeFunc(event.target));
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+// Функция удаления карточки
+function removeCard(target) {
+  target.closest('.card').remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach( (item) => placesList.append(addCard(item, removeCard)) );
